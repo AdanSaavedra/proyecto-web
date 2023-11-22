9645c0de875d34cb77c4355770f32b9907a37a1c
Adan Saavedra <adansaavedraa@icloud.com>
2023-11-22 00:11:00 -0500

    change close button

--- src/Components/DetalleSolicitud/index.jsx
+++ src/Components/DetalleSolicitud/index.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import "./DetalleSolicitud.css";
+import { IoCloseCircle } from "react-icons/io5";
 import { CardContext } from "../../CardContext";
 
 const DetalleSolicitud = () => {
@@ -12,7 +13,7 @@ const DetalleSolicitud = () => {
           <p className="nombre">Nombre Apellido</p>
           <p>
             <span className="cardTags">Motivo: </span>
-            {context.modalData.motivo}
+            {context.modalData.desc}
           </p>
           <p>
             <span className="cardTags">Estatus actual:</span>{" "}
@@ -30,11 +31,12 @@ const DetalleSolicitud = () => {
         </div>
         <div className="detalle">
           <p>
-            <span className="cardTags">Detalle:</span> {context.modalData.desc}
+            <span className="cardTags">Detalle:</span>{" "}
+            {context.modalData.motivo}
           </p>
         </div>
         <span className="btnClose" onClick={() => context.handleModal()}>
-          X
+          <IoCloseCircle />
         </span>
       </section>
     </div>
